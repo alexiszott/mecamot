7d5afe39797b351ae54118da411e4454e459e768
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2024-05-25 15:58:20 +0200

    modif

---
+++ dist/js/send_email.js
@@ -0,0 +1,19 @@
+function emailSend(){
+
+    let params = {
+        name_contact : document.getElementById('name_contact').value,
+        email_contact : document.getElementById('email_contact').value,
+        texte_contact : document.getElementById('texte_contact').value,
+    }
+
+    console.log(params);
+
+    emailjs.send('service_s8v0vlu','template_py0w2kv', params).then(
+        (response) => {
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            console.log('FAILED...', error);
+        },
+    )
+}
