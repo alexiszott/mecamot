31c3212848606dbc502d9aa09bae38ad6f86bef7
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2024-06-02 10:00:34 +0200

    modify emailjs methods

--- dist/js/send_email.js
+++ dist/js/send_email.js
@@ -1,17 +1,37 @@
-function emailSend(){
+document.addEventListener('DOMContentLoaded', function() {
+    let form = document.getElementById('contact-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailSend();
+        clearInput();
+    });
+});
 
-    let params = {
-        name_contact : document.getElementById('name_contact').value,
-        email_contact : document.getElementById('email_contact').value,
-        texte_contact : document.getElementById('texte_contact').value,
-    }
+function emailSend() {
+    let data = {
+        service_id: 'service_s8v0vlu',
+        template_id: 'template_py0w2kv',
+        user_id: 'EAOlP_KtZFgh-f39I',
+        template_params: {
+            'name_contact': document.getElementById('name_contact').value,
+            'email_contact': document.getElementById('email_contact').value,
+            'texte_contact': document.getElementById('texte_contact').value,
+        }
+    };
 
-    emailjs.send('service_s8v0vlu','template_py0w2kv', params).then(
-        (response) => {
-            alert("Email envoyé.");
-        },
-        (error) => {
-            alert("Une erreur s'est produite. Veuillez réessayer.");
-        },
-    )
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function() {
+        $('#valideEmail').modal('show');
+    }).fail(function(error) {
+        $('#errorEmail').modal('show');
+    });
+}
+
+function clearInput(){
+    document.getElementById("name_contact").value = "";
+    document.getElementById("email_contact").value = "";
+    document.getElementById("texte_contact").value = "";
 }
